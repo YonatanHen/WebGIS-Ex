1e3c4191d86141ba666ece604a1c2d6ed29d9ee3
YehonatanHen <yonatan.h333@gmail.com>
2022-02-13 13:44:30 +0200

    .

--- webgis-ex1/src/Components/maps/Polygon.tsx
+++ webgis-ex1/src/Components/maps/Polygon.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import Guide from '../guide';
-import Menu from '../Menu';
+import React from 'react'
+import Guide from '../guide'
+import Menu from '../Menu'
 
-class Polygon extends React.Component <any, any> {
-  constructor(props: any) {
-    super(props)
-    this.state = {
-      guidance:<>
-      <b>Draw polygon:</b> click on the top-right square symbol inside the map, then draw the desired polygon - 
-      click on the left mouse button to mark his edges, double click will draw the last edge.<br/>
-      <b>Erase polygon:</b> Click on the trash icon in the top-right inside the map. <br/>
-      <b>Relocate polygon: </b> When polygon is appearing on the map, be aware about the color
-      of the polygon - if it's blue, click on him and then you can relocate it to new place.<br/>
-      <b>Modify polygon: </b> click on the dots in the middle of polygon sides to resize polygon, to delete dot, click on
-      the desired edge and then on delete button in the keyboard.
-      </>
-    }
-  }
+class Polygon extends React.Component<any, any> {
+	constructor(props: any) {
+		super(props)
+		this.state = {
+			guidance: (
+				<>
+					<b>Draw polygon:</b> click on the top-right square symbol inside the
+					map, then draw the desired polygon - click on the left mouse button to
+					mark his edges, double click will draw the last edge.
+					<br />
+					<b>Erase polygon:</b> Click on the trash icon in the top-right inside
+					the map. <br />
+					<b>Relocate polygon: </b> When polygon is appearing on the map, be
+					aware about the color of the polygon - if it's blue, click on him and
+					then you can relocate it to new place.
+					<br />
+					<b>Modify polygon: </b> click on the dots in the middle of polygon
+					sides to resize polygon, to delete dot, click on the desired edge and
+					then on delete button in the keyboard.
+				</>
+			),
+		}
+	}
 
-  componentDidMount () {
-    const code = `mapboxgl.accessToken = 'pk.eyJ1IjoieW9uYTMzMyIsImEiOiJja2ZuamNodzAwZG92MnltcTl5eG12NGl4In0.dfVOhCUagbvm7ZXCGlEbDQ';
+	componentDidMount() {
+		const code = `mapboxgl.accessToken = 'pk.eyJ1IjoieW9uYTMzMyIsImEiOiJja2ZuamNodzAwZG92MnltcTl5eG12NGl4In0.dfVOhCUagbvm7ZXCGlEbDQ';
     var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/streets-v11', //hosted style id
@@ -69,32 +77,26 @@ class Polygon extends React.Component <any, any> {
     for (var i = 0; i < inputs.length; i++) {
     inputs[i].onclick = switchLayer;
     }
-`;
-    new Function(code)();
-  }
+`
+		new Function(code)()
+	}
 
-
-  render() {
-    return (
-        <>
-          <div className="flex-wrapper">
-          <Guide guidance = {this.state.guidance}/>
-<<<<<<< HEAD:webgis-ex1/src/Components/maps/Polygon.jsx
-            <Menu/>
-          </div>
-          <div class="calculation-box">
-=======
-          <Menu/>
-          <div className="calculation-box">
->>>>>>> Yonatan:webgis-ex1/src/Components/maps/Polygon.tsx
-              <p>
-                Draw a polygon using the draw tools.
-                <div id="calculated-area"></div>
-              </p>
-          </div>
-         </>
-    )
-  }
+	render() {
+		return (
+			<>
+				<div className='flex-wrapper'>
+					<Guide guidance={this.state.guidance} />
+					<Menu />
+					<div className='calculation-box'>
+						<p>
+							Draw a polygon using the draw tools.
+							<div id='calculated-area'></div>
+						</p>
+					</div>
+				</div>
+			</>
+		)
+	}
 }
 
-export default Polygon;
+export default Polygon
